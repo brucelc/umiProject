3740c5296b6288e8ca6a2a49ed7fd64fb4bf11e2
lichuan.lc <lichuan.lc@alibabapictures.com>
2019-07-26 17:41:56 +0800

    WIP: 测试publicPath

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from 'umi/link';
 
 import styles from './index.less';
 
-window.publicPath = window.YLB_CONFIG.assets;
+// window.publicPath = window.YLB_CONFIG.assets;
 export default class App extends PureComponent {
 
   render() {
