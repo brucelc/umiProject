a2361805dd7258ec148c5b87531284c7b9d8099e
lichuan.lc <lichuan.lc@alibabapictures.com>
2019-10-01 00:03:28 +0800

    测试docker-compose

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default class App extends PureComponent {
     return (
       <div className={styles.normal}>
         <div className={styles.welcome} />
-        <Link to="/products">go to /product</Link><br />
+        <Link to="/products">go to /products</Link><br />
         <Link to="/login">go to /login</Link>
         <ul className={styles.list}>
           <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
